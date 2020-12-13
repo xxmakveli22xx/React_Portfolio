9abdc006999ac6d31fa24ff56c2f56205b4d1a16
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-12-12 22:15:21 -0600

    get a page to render

---
+++ src/components/main/index.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+// import Navbar from 'react-bootstrap/Navbar';
+// import Nav from 'react-bootstrap/Nav';
+
+
+ export default function Main() {
+ 
+      return (
+        <>
+          
+          <h1>Hello World inside the Main folder</h1>
+       </>
+      );
+   
+  }
